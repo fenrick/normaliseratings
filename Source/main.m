c3dc5f0d7618376918aefa3f9386de5ff15adde3
Jolyon Suthers <jolyon.suthers@gmail.com>
2010-12-01 10:11:19 +1000

    default xcode project

---
+++ Source/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  NormaliseRatings
+//
+//  Created by Jolyon Suthers on 1/12/10.
+//  Copyright 2010 Griffith University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    // insert code here...
+    NSLog(@"Hello, World!");
+    [pool drain];
+    return 0;
+}
+
